20abae57636da0e65933008f08bb9dee4ccaf4cb
James Giang <james.l.giang@gmail.com>
2014-07-10 13:14:20 +1000

    brew install/clean scripts

--- install.sh
+++ install.sh
@@ -7,10 +7,14 @@ git submodule update
 echo "Deleting the old files"
 rm ~/.vimrc
 rm -rf ~/.vim
+rm ~/bin/brew_clean
+rm ~/bin/brew_install
 
 echo "Symlinking files"
 ln -s ~/github/dotfiles/vimrc ~/.vimrc
 ln -s ~/github/dotfiles/vim ~/.vim
+ln -s ~/github/dotfiles/scripts/brew_clean ~/bin/brew_clean
+ln -s ~/github/dotfiles/scripts/brew_install ~/bin/brew_install
 
 echo "Updating submodules"
 git submodule foreach git pull origin master --recurse-submodules
